ed8f05024aa798a4b66380526f908bb8ec10505f
TOSHIBA-PC\TOSHIBA <ing.nequiz@hotmail.com>
2018-01-17 18:01:44 -0600

    add web

--- AppNex/src/test/TestWikipedia.java
+++ AppNex/src/test/TestWikipedia.java
@@ -15,15 +15,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  * @author Nex
  */
 public class TestWikipedia {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws InterruptedException {
        
    WebDriver driver = new FirefoxDriver();
    driver.get("http://www.wikipedia.org");  //obtener el enlace deseado
    WebElement link;
-   link = driver.findElement(By.LinkText("English"));  //busca el elemento enla pagina web
+   link = driver.findElement(By.linkText("English"));  //busca el elemento enla pagina web
    link.click();
    Thread.sleep(5000);                //tiempo a esperar en lo que carga 
-   
+   WebElement searchBox; //Crear un Elemento ala caja del texto de busqueda
+   searchBox = driver .findElement(By.id("searchInput"));
    
    }  
 }
