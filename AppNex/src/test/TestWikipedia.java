8b5d25652d50b8dac3da5436446e759c17eb3952
TOSHIBA-PC\TOSHIBA <ing.nequiz@hotmail.com>
2018-01-19 11:23:51 -0600

    Rename

--- AppNex/src/test/TestWikipedia.java
+++ AppNex/src/test/TestWikipedia.java
@@ -3,9 +3,9 @@ package test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 /**
  * @author Nex
@@ -13,17 +13,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class TestWikipedia {
    public static void main(String[] args) throws InterruptedException {
        
-  // WebDriver driver = new FirefoxDriver();
-   System.setProperty("webdriver.ie.driver", "C:\\Users\\TOSHIBA\\Downloads\\chromedriver.exe");
-   WebDriver driver  = new ChromeDriver();
+   // WebDriver driver = new FirefoxDriver();
+   System.setProperty("webdriver.ie.driver", "C:\\Users\\TOSHIBA\\Downloads\\IEDriverServer.exe");
+   //WebDriver driver  = new ChromeDriver();
+	   WebDriver driver = new InternetExplorerDriver(); 
    
-   driver.get("http://www.wikipedia.org");  //obtener el enlace deseado
+   driver.get("http://www.wikipedia.org");            /*obtener el enlace deseado*/
    WebElement link;
        System.out.println(driver.getPageSource());     //obtener el codigofuetne de una pagina
-   link = driver.findElement(By.linkText("Español"));  //busca el elemento enla pagina web
+   link = driver.findElement(By.linkText("Español"));  //Basic el elemento enla pagina web
    link.click();
    Thread.sleep(5000);                //tiempo a esperar en lo que carga 
-   WebElement searchBox;             //Crear un Elemento ala caja del texto de busqueda
+   WebElement searchBox;              //Crear un Elemento ala caja del texto de busqueda
    searchBox = driver .findElement(By.id("searchInput"));     //Cuadro de  busqueda
    searchBox.sendKeys("Software");     //Se escribe en el cuadro de busqueda 
    searchBox.submit();
---
+++ Hilo/src/hilo/Cliente.java
@@ -0,0 +1,21 @@
+package hilo;
+
+public class Cliente {
+	private String nombre;
+	private int[] carroCompra;
+	public Cliente(String string, int[] is) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Object getCarroCompra() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	// Constructor, getter y setter
+
+}
