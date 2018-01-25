332983982a1184dd29aa353e511abc407f99cac1
TOSHIBA-PC\TOSHIBA <ing.nequiz@hotmail.com>
2018-01-25 12:39:43 -0600

    proyecto prueba
    se realizan rpeubas en un nuevo proyecto de java

--- SeleniumCorreo/src/seleniumcorreo/SeleniumCorreo.java
+++ SeleniumCorreo/src/seleniumcorreo/SeleniumCorreo.java
@@ -13,15 +13,9 @@ public class SeleniumCorreo {
     // Optional, if not specified, WebDriver will search your path
     System.setProperty("webdriver.chrome.driver", "C:\\Users\\TOSHIBA\\Downloads\\chromedriver.exe"); 
          WebDriver driver = new ChromeDriver();
-   driver.get("http://www.google.com/xhtml");         /*obtener el enlace deseado*/
-   //System.out.println(driver.getPageSource());     //obtener el codigofuetne de una pagina
-   //Thread.sleep(1000);                             //tiempo a esperar en lo que carga 
-           
-   //Crear un Elemento ala caja del texto de busqueda
-    driver.findElement(By.id("lst-ib")).clear();
-    driver.findElement(By.id("lst-ib")).sendKeys("selenium");
-    driver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
-    driver.findElement(By.linkText("Selenium - Wikipedia, la enciclopedia libre")).click();
+         driver.get("http://www.google.com/xhtml");         /*obtener el enlace deseado*/
+   
+
     
    driver.quit();                      //Cerrar la instancia del navegador en Web drive 
    
---
+++ SeleniumPrueba/src/seleniumprueba/SeleniumPrueba.java
@@ -0,0 +1,35 @@
+
+package seleniumprueba;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class SeleniumPrueba {
+    
+       public static void main(String[] args) throws InterruptedException {
+       
+
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\TOSHIBA\\Downloads\\chromedriver.exe"); 
+         WebDriver driver = new ChromeDriver();
+   driver.get("http://www.google.com/xhtml");         /*obtener el enlace deseado*/
+   
+           
+   //Crear un Elemento ala caja del texto de busqueda
+    driver.findElement(By.id("lst-ib")).clear();
+    
+    driver.findElement(By.id("lst-ib")).sendKeys("selenium");
+    driver.findElement(By.name("btnK")).click();                                //da lick en el boton buscar
+    driver.findElement(By.linkText("Selenium - Wikipedia, la enciclopedia libre")).click();
+    //driver.findElement(By.cssSelector("li.toclevel-2.tocsection-7 > a > span.toctext")).click();   dezplazala pagina web
+    driver.findElement(By.linkText("Última versión estable")).click();
+    driver.findElement(By.linkText("wikificado")).click();
+
+    
+   driver.quit();                      //Cerrar la instancia del navegador en Web drive 
+   
+       }  
+
+
+}
