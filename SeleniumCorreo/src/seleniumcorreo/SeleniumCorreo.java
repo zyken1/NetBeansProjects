cac4d6ac94eae793630af5e4ed7c404b1d508ca5
TOSHIBA-PC\TOSHIBA <ing.nequiz@hotmail.com>
2018-01-25 18:12:20 -0600

    correo Listo
    En este codigo se logra acceder a el correo satisfactoriamente

--- SeleniumCorreo/src/seleniumcorreo/SeleniumCorreo.java
+++ SeleniumCorreo/src/seleniumcorreo/SeleniumCorreo.java
@@ -14,17 +14,18 @@ public class SeleniumCorreo {
     System.setProperty("webdriver.chrome.driver", "C:\\Users\\TOSHIBA\\Downloads\\chromedriver.exe"); 
          WebDriver driver = new ChromeDriver();
          //  Wait For Page To Load
-        // Put a Implicit wait, this means that any search for elements on the page
+         // Put a Implicit wait, this means that any search for elements on the page
          driver.get("https://mail.google.com/");
          driver.manage().window().maximize();
-    driver.findElement(By.cssSelector("a.maia-button.button-download.mobile-is-hidden.get-started.go-to-drive")).click();
+    //driver.findElement(By.cssSelector("a.maia-button.button-download.mobile-is-hidden.get-started.go-to-drive")).click();
     //driver.findElement(By.id("Email")).sendKeys("atencion3goldenharvest@gmail.com");
     //driver.findElement(By.cssSelector("span > svg")).click();
     //driver.findElement(By.cssSelector("#identifierLink > div.vdE7Oc.f3GIQ > p")).click();
-    driver.findElement(By.linkText("Iniciar sesión")).click();
-    driver.findElement(By.cssSelector("span.RveJvd.snByac")).click();
+    //driver.findElement(By.linkText("Iniciar sesión")).click();
+   //driver.findElement(By.cssSelector("span.RveJvd.snByac")).click();
     driver.findElement(By.id("identifierId")).sendKeys("atencion3goldenharvest@gmail.com");
     driver.findElement(By.cssSelector("span.RveJvd.snByac")).click();
+    
     driver.findElement(By.name("password")).clear();
     driver.findElement(By.name("password")).sendKeys("goldenupn@#33");
     driver.findElement(By.cssSelector("span.RveJvd.snByac")).click();   
