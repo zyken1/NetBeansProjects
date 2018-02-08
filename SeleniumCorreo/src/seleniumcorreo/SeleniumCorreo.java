97ae7003c47a2c38357c650fa26e91acaf54f21b
TOSHIBA-PC\TOSHIBA <ing.nequiz@hotmail.com>
2018-02-08 16:54:23 -0600

    se cambia la ruta y se selecciona el correspondiente

--- SeleniumCorreo/src/seleniumcorreo/SeleniumCorreo.java
+++ SeleniumCorreo/src/seleniumcorreo/SeleniumCorreo.java
@@ -9,37 +9,34 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumCorreo {
     
+
        public static void main(String[] args) throws InterruptedException {
        
     // Optional, if not specified, WebDriver will search your path
-    System.setProperty("webdriver.chrome.driver", "C:\\Users\\TOSHIBA\\Downloads\\chromedriver.exe"); 
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\TOSHIBA\\Downloads\\Selenium\\chromedriver.exe"); 
          WebDriver driver = new ChromeDriver();
          //  Wait For Page To Load
-         // Put a Implicit wait, this means that any search for elements on the page
+ 
+         
          driver.get("https://mail.google.com/");
          driver.manage().window().maximize();
     
     //driver.findElement(By.id("Email")).sendKeys("atencion3goldenharvest@gmail.com");
     driver.findElement(By.id("identifierId")).sendKeys("atencion3goldenharvest@gmail.com");
     
-    /*var value = this.browserbot.findElement("id=Email").value;
-        if (value == "Email") {
-        driver.findElement(By.id("Email")).sendKeys("atencion3goldenharvest@gmail.com");
-        Else
-                {
-                }
-    
-    }
+   var value = this.browserbot.findElement("id=someInput").value;
+    if (value == "Slanec is the best!") {
+    this.browserbot.findElement("id=someButton").click();
+}
 
-*/
+    
+    
     //driver.findElement(By.id("identifierId")).sendKeys(Keys.ENTER);             //detecta la entrada de teclas
     driver.findElement(By.cssSelector("span.RveJvd.snByac")).click();             //busqueda del boton siguiente
-    driver.findElement(By.name("password")).clear();
-    driver.findElement(By.name("password")).sendKeys("goldenupn@#33");
+    driver.findElement(By.name("hiddenPassword")).sendKeys("goldenupn@#33");
     driver.findElement(By.cssSelector("span.RveJvd.snByac")).click();   
  
         //dentro del correo
-    //driver.findElement(By.cssSelector("span.RveJvd.snByac")).click();
     driver.findElement(By.xpath("(//span[@name='SOLUCIONES ENERGETICAS'])[2]")).click();
     driver.findElement(By.linkText("Recibidos")).click();
     
@@ -48,6 +45,8 @@ public class SeleniumCorreo {
      driver.quit();          //Cerrar la instancia del navegador en Web drive 
    
        }  
-
-
 }
+
+
+
+
